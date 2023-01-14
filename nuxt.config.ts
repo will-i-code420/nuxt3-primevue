3021e444066c1e24eb5e710439c204d67a5f8640
Will Aronow <waronow123@gmail.com>
2023-01-14 14:29:23 -0700

    first commit, installed primevue,icons,flex and created start of plugin

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  css: [
+        'primevue/resources/themes/saga-blue/theme.css',
+        'primevue/resources/primevue.css',
+        'primeflex/primeflex.css',
+        'primeicons/primeicons.css'
+    ],
+	build: {
+		transpile: ['primevue']
+	}
+})
