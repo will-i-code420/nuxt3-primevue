39b100a054c02f2e45fcd6d63fea7ee80e5059c1
Will Aronow <waronow123@gmail.com>
2023-01-14 19:36:23 -0700

    added basic hero component from freeblock

---
+++ components/TheHero.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div class="grid grid-nogutter surface-section text-800">
+    <div class="col-12 md:col-6 p-6 text-center md:text-left flex align-items-center ">
+      <section>
+        <span class="block text-6xl font-bold mb-1">Create the screens your</span>
+        <div class="text-6xl text-primary font-bold mb-3">your visitors deserve to see</div>
+        <p class="mt-0 mb-4 text-700 line-height-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+
+        <Button label="Learn More" type="button" class="mr-3 p-button-raised"></Button>
+        <Button label="Live Demo" type="button" class="p-button-outlined"></Button>
+      </section>
+    </div>
+    <div class="col-12 md:col-6 overflow-hidden">
+      <img src="images/blocks/hero/hero-1.png" alt="Image" class="md:ml-auto block md:h-full" style="clip-path: polygon(8% 0, 100% 0%, 100% 100%, 0 100%)">
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
